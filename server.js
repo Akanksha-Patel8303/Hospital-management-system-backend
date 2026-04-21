75f8d7f63b9f73e60e702e559aed65bef7c96629
Akanksha-Patel8303 <vipin1patel123@gmail.com>
2026-04-22 01:22:59 +0530

    fix root route

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("🚀 Hospital Backend Running");
+});
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
